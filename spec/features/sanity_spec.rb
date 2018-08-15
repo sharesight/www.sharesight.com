dff4ed27ba0729071cfb825e2958f07215730cf5
Kylor Hall <kylor@sharesight.com>
2018-08-15 12:47:49 +1200

    Replace favicon.

--- spec/features/sanity_spec.rb
+++ spec/features/sanity_spec.rb
@@ -35,6 +35,8 @@ describe 'Build Sanity', :type => :feature do
 
     # files only in root directory
     expect(root_dir).to include("favicon.ico")
+    expect(root_dir).to include("favicon-16x16.png")
+    expect(root_dir).to include("favicon-32x32.png")
     expect(root_dir).to include("robots.txt")
     expect(root_dir).to include("sitemap.xml")
 
