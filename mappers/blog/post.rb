e831b674f7544da19f36f8026cf270fd8bc8a3ae
Kylor Hall <kylor@sharesight.com>
2018-09-20 19:54:51 +1200

    Cleanup comments; allow for `slug` to exist, don't overwrite.

--- mappers/blog/post.rb
+++ mappers/blog/post.rb
@@ -1,9 +1,8 @@
-# This normalizes the data coming from contentful, so a user enters ' Great   Title!' and it becomes 'Great Title!'
-# Adds url slugs.
+# This normalizes the data coming from contentful.
 class BlogPostMapper < ContentfulMiddleman::Mapper::Base
   def map(context, entry)
     super
-    context.title = context.title&.to_s.squeeze(' ').strip
-    context.slug = context.title&.downcase.urlize.squeeze('-')
+    context.title = context.title&.to_s.squeeze(' ').strip # An author enters ' Great   Title!' and it becomes 'Great Title!'
+    context.slug = context.slug || context.title&.downcase.urlize.squeeze('-') # Adds url slugs.
   end
 end
