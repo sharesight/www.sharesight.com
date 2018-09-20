7915cdcef6edbcbb2a6a9cf490e6b65f968e3bda
Kylor Hall <kylor@sharesight.com>
2018-09-20 19:56:15 +1200

    Protect slug (it doesn't currently exist).

--- mappers/blog/post.rb
+++ mappers/blog/post.rb
@@ -2,7 +2,12 @@
 class BlogPostMapper < ContentfulMiddleman::Mapper::Base
   def map(context, entry)
     super
+
+    keys = entry.fields_with_locales.keys
+    unless keys.include?(:slug)
+      context.slug = context.title&.downcase.urlize.squeeze('-') # Adds url slugs if they don't exist.
+    end
+
     context.title = context.title&.to_s.squeeze(' ').strip # An author enters ' Great   Title!' and it becomes 'Great Title!'
-    context.slug = context.slug || context.title&.downcase.urlize.squeeze('-') # Adds url slugs.
   end
 end
