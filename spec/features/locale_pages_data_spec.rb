88bb341d7448507319608ad247aea930576885b0
Thorsten Boettger <boettger@mt7.de>
2019-02-12 11:53:56 +1300

    Make tests faster

--- spec/features/locale_pages_data_spec.rb
+++ spec/features/locale_pages_data_spec.rb
@@ -1,67 +1,51 @@
 require 'spec_helper'
 
 describe 'Locale Pages', :type => :feature do
+
   Capybara.app.data.locales.each do |locale|
     it "should load for locale #{locale['id']}" do
       locale.pages.each do |page_data|
         visit localize_path(page_data[:page], locale_id: locale[:id])
 
         expect(page).to respond_successfully
+        expect_basic_meta(page, page_data)
+        expect_canonical_urls(page, page_data, locale)
+        expect_taglines(page, page_data, locale)
+        expect_footer(page)
       end
     end
   end
 
-  Capybara.app.data.locales.each do |locale|
-    it "should have expected meta tags for locale #{locale['id']}" do
-      locale.pages.each do |page_data|
-        visit localize_path(page_data[:page], locale_id: locale[:id])
-        base_page_data = base_locale_page(page_data[:page])
+  private
 
-        expect(page).to have_base_metas()
-        expect(page).to have_social_metas()
-        expect(page).to have_titles(page_data[:page_title], base_page_data[:page_title])
-        expect(page).to have_descriptions(page_data[:page_description], base_page_data[:page_description])
+  def expect_basic_meta(page, page_data)
+    base_page_data = base_locale_page(page_data[:page])
+    expect(page).to have_base_metas()
+    expect(page).to have_social_metas()
+    expect(page).to have_titles(page_data[:page_title], base_page_data[:page_title])
+    expect(page).to have_descriptions(page_data[:page_description], base_page_data[:page_description])
+  end
+
+  def expect_canonical_urls(page, page_data, locale)
+    expect(page).to have_meta('og:url', base_url(page_data[:page]), name_key: 'property')
+    expect(page).to have_head('link', args: { rel: 'canonical', href: localize_url(page_data[:page], locale_id: locale[:id])}, debug: :href)
+
+    Capybara.app.page_alternative_locales(page_data[:page]).each do |alternate_locale|
+      if alternate_locale[:id] == default_locale_id
+        expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(page_data[:page], locale_id: default_locale_id), hreflang: 'x-default' }, debug: :href)
       end
+      expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(page_data[:page], locale_id: alternate_locale[:id]), hreflang: alternate_locale[:lang]&.downcase }, debug: :href)
     end
   end
 
-  Capybara.app.data.locales.each do |locale|
-    it "should have expected canonical urls for locale #{locale['id']}" do
-      locale.pages.each do |page_data|
-        visit localize_path(page_data[:page], locale_id: locale[:id])
-
-        expect(page).to have_meta('og:url', base_url(page_data[:page]), name_key: 'property')
-
-        expect(page).to have_head('link', args: { rel: 'canonical', href: localize_url(page_data[:page], locale_id: locale[:id])}, debug: :href)
-        Capybara.app.page_alternative_locales(page_data[:page]).each do |alternate_locale|
-          if alternate_locale[:id] == default_locale_id
-            expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(page_data[:page], locale_id: default_locale_id), hreflang: 'x-default' }, debug: :href)
-          end
-          expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(page_data[:page], locale_id: alternate_locale[:id]), hreflang: alternate_locale[:lang]&.downcase }, debug: :href)
-        end
-      end
+  def expect_taglines(page, page_data, locale)
+    ['tagline', 'footer_tagline'].each do |tag|
+      expect(page).to have_text(page_data[tag]), "No #{tag} on #{locale[:id]}/#{page_data[:page]}." if page_data[tag]
     end
   end
 
-  Capybara.app.data.locales.each do |locale|
-    it "should have taglines where applicable for locale #{locale['id']}" do
-      locale.pages.each do |page_data|
-        visit localize_path(page_data[:page], locale_id: locale[:id])
-
-        ['tagline', 'footer_tagline'].each do |tag|
-          expect(page).to have_text(page_data[tag]), "No #{tag} on #{locale[:id]}/#{page_data[:page]}." if page_data[tag]
-        end
-      end
-    end
+  def expect_footer(page)
+    expect(page).to have_css('.footer__copyright', text: 'All rights reserved.')
   end
 
-  Capybara.app.data.locales.each do |locale|
-    it "should have a footer for locale #{locale['id']}" do
-      locale.pages.each do |page_data|
-        visit localize_path(page_data[:page], locale_id: locale[:id])
-
-        expect(page).to have_css('.footer__copyright', text: 'All rights reserved.')
-      end
-    end
-  end
 end
--- spec/features/partner_featured_page_spec.rb
+++ spec/features/partner_featured_page_spec.rb
@@ -10,68 +10,62 @@ describe 'Partner Category Pages', :type => :feature do
       visit localize_path(@path, locale_id: locale[:id])
 
       expect(page).to respond_successfully
+      expect_basic_tags(page, locale_page('partners', locale))
+      expect_urls(page, locale)
+      expect_elements(page, locale)
     end
   end
 
-  Capybara.app.data.locales.each do |locale|
-    it "should have expected meta tags for locale #{locale['id']}" do
-      visit localize_path(@path, locale_id: locale[:id])
-      current_locale_page = locale_page('partners', locale)
-      base_page = base_locale_page('partners')
+  private
 
-      expect(page).to have_base_metas()
-      expect(page).to have_social_metas()
-      expect(page).to have_titles(current_locale_page[:page_title], base_page[:page_title])
-      expect(page).to have_descriptions(current_locale_page[:page_description], base_page[:page_description])
-    end
+  def expect_basic_tags(page, current_locale_page)
+    base_page = base_locale_page('partners')
+    expect(page).to have_base_metas()
+    expect(page).to have_social_metas()
+    expect(page).to have_titles(current_locale_page[:page_title], base_page[:page_title])
+    expect(page).to have_descriptions(current_locale_page[:page_description], base_page[:page_description])
   end
 
-  Capybara.app.data.locales.each do |locale|
-    it "should have expected urls for locale #{locale['id']}" do
-      visit localize_path(@path, locale_id: locale[:id])
-
-      expect(page).to have_meta('og:url', base_url(@path), name_key: 'property')
-      expect(page).to have_head('link', args: { rel: 'canonical', href: localize_url(@path, locale_id: locale[:id]) }, debug: :href)
-      locales.each do |alternate_locale|
-        if alternate_locale[:id] == default_locale_id
-          expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(@path, locale_id: default_locale_id), hreflang: 'x-default' }, debug: :href)
-        end
-        expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(@path, locale_id: alternate_locale[:id]), hreflang: alternate_locale[:lang].downcase }, debug: :href)
+  def expect_urls(page, locale)
+    expect(page).to have_meta('og:url', base_url(@path), name_key: 'property')
+    expect(page).to have_head('link', args: { rel: 'canonical', href: localize_url(@path, locale_id: locale[:id]) }, debug: :href)
+    locales.each do |alternate_locale|
+      if alternate_locale[:id] == default_locale_id
+        expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(@path, locale_id: default_locale_id), hreflang: 'x-default' }, debug: :href)
       end
+      expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(@path, locale_id: alternate_locale[:id]), hreflang: alternate_locale[:lang].downcase }, debug: :href)
     end
   end
 
-  Capybara.app.data.locales.each do |locale|
-    it "should have the expected elements for locale #{locale['id']}" do
-      categories = get_partners_categories(locale, all: true)
-      visit localize_path(@path, locale_id: locale[:id])
+  def expect_elements(page, locale)
+    categories = get_partners_categories(locale, all: true)
 
-      expect(page).not_to have_css('a.breadcrumb', text: 'Partners')
+    expect(page).not_to have_css('a.breadcrumb', text: 'Partners')
 
-      expect(page).to have_css('h1.partners-header__title', text: 'Partners')
+    expect(page).to have_css('h1.partners-header__title', text: 'Partners')
 
-      # Expect this to have partners.
-      expect(page).to have_selector(:css, 'a.btn', text: 'Learn More')
+    # Expect this to have partners.
+    expect(page).to have_selector(:css, 'a.btn', text: 'Learn More')
 
-      # Check for the navigation on the right.
-      expect(page).to have_css('.partners-header__categories', text: 'Jump To')
-      categories.each do |cat|
-        expect(page).to have_css(
-          ".partners-header__categories-list li a.partners-header__category[href='#{cat[:url]}']",
-          text: cat[:id] == 'all' ? cat[:name] : cat[:title]
-        )
-      end
-
-      categories.each do |cat|
-        expect(page).to have_css(".partner-category__title", text: /^#{cat[:id] == 'all' ? 'Featured Partners' : cat[:name]}/)
-        expect(page).to have_css(
-          ".partner-category__title a.partner-category__browse[href='#{cat[:url]}']",
-          text: "Browse #{cat[:id] == 'all' ? 'All' : cat[:name].singularize()} Partners →".squeeze(' ')
-        )
-      end
-
-      # Have a footer.
-      expect(page).to have_css('.footer__copyright', text: 'All rights reserved.')
+    # Check for the navigation on the right.
+    expect(page).to have_css('.partners-header__categories', text: 'Jump To')
+    categories.each do |cat|
+      expect(page).to have_css(
+        ".partners-header__categories-list li a.partners-header__category[href='#{cat[:url]}']",
+        text: cat[:id] == 'all' ? cat[:name] : cat[:title]
+      )
     end
+
+    categories.each do |cat|
+      expect(page).to have_css(".partner-category__title", text: /^#{cat[:id] == 'all' ? 'Featured Partners' : cat[:name]}/)
+      expect(page).to have_css(
+        ".partner-category__title a.partner-category__browse[href='#{cat[:url]}']",
+        text: "Browse #{cat[:id] == 'all' ? 'All' : cat[:name].singularize()} Partners →".squeeze(' ')
+      )
+    end
+
+    # Have a footer.
+    expect(page).to have_css('.footer__copyright', text: 'All rights reserved.')
   end
+
 end
