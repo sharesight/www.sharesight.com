a5c5b9bb1171f55d7548ed26c99e01e9f872ec51
Kylor Hall <kylor@kylorhall.com>
2020-12-10 13:14:25 +1300

    Remove the FAQ so it can be replaced by a Contentful Landing Page.

--- spec/features/external_sanity_spec.rb
+++ spec/features/external_sanity_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe 'External Link Sanity', type: :feature do
   it 'should return a valid page for hard coded links' do
-    %w(reviews affiliates events become-a-partner).each do |path|
+    [
+      'reviews',
+      'affiliates',
+      'events',
+      'become-a-partner',
+      'faq'
+    ].each do |path|
       visit path
       expect(page).to respond_successfully
     end
