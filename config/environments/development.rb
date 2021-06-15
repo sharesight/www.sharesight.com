976502229b8d7c830bd083a5518616f6779d8bd4
Kyle Beattie <kyle.beattie@sharesight.co.nz>
2021-06-15 18:24:51 +1200

    Changed localized URLs to unlocalized

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ module ApplicationConfig
 	BASE_URL = 'http://localhost:4567'
 	PORTFOLIO_URL = 'https://portfolio.sharesight.com'
 	HELP_URL = 'https://help.sharesight.com'
-	COMMUNITY_URL = 'https://community.sharesight.com'
+	COMMUNITY_URL = 'https://test-community.sharesight.com'
 
 	module GoogleAnalytics
 		TAG_MANAGER_CONTAINER = 'GTM-P2DH5X'
