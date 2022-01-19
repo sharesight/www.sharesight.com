61de8863c616d734f088c0d5124289e5405679ae
Maisie <maisie.embleton@gmail.com>
2022-01-20 11:57:11 +1300

    Revert "Removing references to the middleman pages - Updating tests for events, faq, affiliates"

--- spec/features/external_sanity_spec.rb
+++ spec/features/external_sanity_spec.rb
@@ -4,7 +4,10 @@ describe 'External Link Sanity', type: :feature do
   it 'should return a valid page for hard coded links' do
     [
       'reviews',
+      'affiliates',
+      'events',
       'become-a-partner',
+      'faq'
     ].each do |path|
       visit path
       expect(page).to respond_successfully
