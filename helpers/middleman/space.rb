7ba262e44b9ebc81a2fc2c614559210c908fa158
Maisie <maisie.embleton@sharesight.co.nz>
2022-08-01 12:59:52 +0800

    Removing references to partners page

--- helpers/middleman/space.rb
+++ helpers/middleman/space.rb
@@ -9,28 +9,4 @@ module MiddlemanSpaceHelpers
 
     false
   end
-
-  def space_category_title
-    # NOTE: the blog has been removed, though this check is there to ensure it's the partners page
-    space = if current_path_array.any?{ |path| /^partners/ =~ path }
-      'Partners'
-    end
-    # The locals.category is passed here by pagination proxy via the locals hash.
-    category = current_page.metadata[:locals][:category]
-    return space unless is_category_page?
-    return "#{category[:name]} Partners" if space&.downcase == 'partners'
-    category[:name]
-  end
-
-  def space_category_page_title
-    return locale_page[:page_title] unless is_category_page?
-
-    "#{space_category_title} | #{current_locale_obj[:append_title]}"
-  end
-
-  def base_space_category_page_title
-    return base_locale_page[:page_title] || locale_page[:page_title] unless is_category_page?
-
-    "#{space_category_title} | #{default_locale_obj[:append_title]}"
-  end
 end
